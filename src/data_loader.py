30a29452a96e001db1b006758f0ce0d4ff304cd2
Bram Veenman <bramveenman@gmail.com>
2026-01-19 02:55:14 +0100

    improve limit logic

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,16 +1,11 @@
 import pandas as pd
 from datasets import load_dataset
 
-def load_wmt_data(lang, limit=False):
+def load_wmt_data(lang, limit):
     ds = load_dataset("google/wmt24pp", lang)
     df = pd.DataFrame(ds['train'])
     
-    if limit:
-        # only use part of data
-        sources = df['source'].tolist()[:limit]
-        targets = df['target'].tolist()[:limit]
-    else:
-        sources = df['source'].tolist()
-        targets = df['target'].tolist()
+    sources = df['source'].tolist()[:limit]
+    targets = df['target'].tolist()[:limit]
     
     return sources, targets
