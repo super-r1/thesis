1c195706b5a9d846d679baf2b8048bc7d67ccac0
Bram Veenman <bramveenman@gmail.com>
2026-01-21 00:45:31 +0100

    add training pipeline and environment setup

---
+++ job.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=translate_gemma
+#SBATCH --partition=gpu_a100
+#SBATCH --gpus=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --time=04:00:00
+#SBATCH --output=logs/%j.out
+
+# load environment
+module purge
+module load 2024
+module load CUDA/12.6.0
+
+export PYTHONWARNINGS="ignore:pkg_resources is deprecated"
+
+# set up scratch storage for faster performance
+SCRATCH_DIR="/scratch-shared/$USER/thesis_run_$SLURM_JOB_ID"
+export HF_HOME="/scratch-shared/$USER/hf_cache"
+mkdir -p "$SCRATCH_DIR"
+mkdir -p "$HF_HOME" 
+cp -r . "$SCRATCH_DIR"
+cd "$SCRATCH_DIR"
+
+# activate conda environment
+source activate translate-gemma
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
+# run code
+echo "Starting Gemma Translation at $(date)"
+python main.py
+
+# save results
+echo "Moving results to Home directory..."
+TIMESTAMP=$(date +"%Y%m%d_%H%M")
+cp -r outputs/* "$SLURM_SUBMIT_DIR/outputs/"
+
+echo "Job completed at $(date)"
---
+++ train.py
@@ -0,0 +1,96 @@
+import os
+import torch
+from datasets import Dataset
+from peft import LoraConfig
+from transformers import AutoModelForImageTextToText, AutoProcessor, TrainingArguments
+from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
+from src import load_model_and_processor, load_flores_data
+from src.config import OUTPUT_DIR
+
+# load model and processor
+model, processor = load_model_and_processor()
+
+# set padding
+processor.tokenizer.padding_side = "right"
+processor.tokenizer.pad_token = processor.tokenizer.eos_token
+
+# create huggingface Dataset object for flores dataset
+# follows the specific requirements for Gemma prompts
+def get_formatted_dataset():
+    # get "raw" data
+    sources, targets = load_flores_data()
+
+    formatted_data = []
+    for s, t in zip(sources, targets):
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text", 
+                        "text": s,
+                        "source_lang_code": "en",
+                        "target_lang_code": "nl",
+                    }
+                ]
+            },
+            {
+                "role": "assistant",
+                "content": t
+            }
+        ]
+        
+        # apply template
+        text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=False)
+        formatted_data.append({"text": text})
+    
+    return Dataset.from_list(formatted_data)
+
+train_dataset = get_formatted_dataset()
+
+# mask user prompt to prevent training on it
+response_template_ids = processor.tokenizer.encode("<start_of_turn>model", add_special_tokens=False)
+collator = DataCollatorForCompletionOnlyLM(
+    response_template=response_template_ids, 
+    tokenizer=processor.tokenizer
+)
+
+# lora config
+lora_config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    target_modules="all-linear",
+    task_type="CAUSAL_LM",
+    lora_dropout=0.05,
+)
+
+# trainer args
+training_args = TrainingArguments(
+    output_dir=OUTPUT_DIR,
+    per_device_train_batch_size=4,
+    gradient_accumulation_steps=4,
+    learning_rate=2e-4,
+    bf16=torch.cuda.is_available(),
+    logging_steps=10,
+    num_train_epochs=3,
+    save_strategy="epoch",
+    remove_unused_columns=True,
+    gradient_checkpointing=True,
+    optim="adamw_torch_fused"
+#    dataloader_num_workers=4,
+)
+
+# create trainer
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=train_dataset,
+    dataset_text_field="text",
+    formatting_func=lambda x: x["text"],
+    max_seq_length=1024,
+    data_collator=collator,
+    tokenizer=processor.tokenizer,
+    args=training_args,
+    peft_config=lora_config,
+)
+
+trainer.train()
